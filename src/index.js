4e62a1dea14778af0f881259e4bad9663fc5a07c
Hassan Shah <syed.ulhassan@cirvianm.cat>
2023-12-20 22:29:05 +0100

    finish

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const sharp = require('sharp');
+const sharp = require("sharp");
 
-sharp("../img/tpv-mb.png")
+sharp("../img/collsacampus-mb.png")
   .png({ quality: 75, compressionLevel: 7 })
-  .toFile("tpv-mb.webp", (err, info) => {
+  .toFile("../img/collsacampus-mb.webp", (err, info) => {
     if (err) {
       console.error(err);
     } else {
